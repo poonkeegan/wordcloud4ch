d69a701fd88dfe8d59372459da90ebfcf9aefba0
Keegan Poon <poonkeegan@gmail.com>
2019-01-20 02:36:29 -0500

    test json

--- project.clj
+++ project.clj
@@ -3,7 +3,8 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [org.clojure/data.json "0.2.6"]]
   :plugins [[lein-cljfmt "0.6.4"]
             [nrepl/lein-nrepl "0.3.2"]]
   :main ^:skip-aot wordcloud4ch.core
--- src/wordcloud4ch/core.clj
+++ src/wordcloud4ch/core.clj
@@ -1,7 +1,12 @@
 (ns wordcloud4ch.core
-  (:gen-class))
+  (:gen-class)
+  (:require [clojure.data.json :as json]))
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+
+(json/read-str (slurp "https://a.4cdn.org/a/catalog.json")
+               :key-fn keyword)
